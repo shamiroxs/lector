4d0c328d298bc599ad3b2b89eb25be37aa679837
Shamir Asharf <shamirkolakkadan26@gmail.com>
2024-12-30 20:06:05 +0530

    Update xtts_server.py

--- xtts_server.py
+++ xtts_server.py
@@ -101,5 +101,5 @@ def synthesize_text():
     return send_file("combined_output.mp3", mimetype='audio/mpeg', as_attachment=True, download_name='output.mp3')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
 
